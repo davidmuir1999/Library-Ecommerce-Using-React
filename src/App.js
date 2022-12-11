5713db1aa7d7a4ebca8303c811720bbb24fe1fc5
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-12-11 16:49:35 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LandingPage from "./components/LandingPage";
+import Nav from "./components/Nav";
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
